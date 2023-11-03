de145c82d5d814b00934897aabbcdfb686f2add6
negnq <117580177+neginashkan@users.noreply.github.com>
2023-11-03 16:05:33 +0330

    Update Page.jsx

--- src/Page.jsx
+++ src/Page.jsx
@@ -6,6 +6,7 @@ import LoginCreatAccount from "./Components/LoginCreatAccount"
 export default function Page() {
     return (
         <div>
+            {/* add header here */}
             <LoginCreatAccount />
         </div>
     );

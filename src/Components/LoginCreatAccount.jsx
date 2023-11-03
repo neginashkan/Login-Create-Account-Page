22ab6e9d403cc815950565cde20ec123d8d36c27
negnq <117580177+neginashkan@users.noreply.github.com>
2023-11-03 16:02:17 +0330

    write the CSS ,styles , add font ,fix sum bugs on Login Create Account component

--- src/Components/LoginCreatAccount.jsx
+++ src/Components/LoginCreatAccount.jsx
@@ -3,13 +3,13 @@ import "../CSS/styles.css";
 export default function LoginCreatAccount() {
     return (
         <div className="login-creat-account-container">
-            <div className="login-creat-account-link">
-                <a href="#" rel="noopener noreferrer" className="creat-account-link">ثبت نام</a>
+            <div className="login-creat-account-links">
                 <a href="#" rel="noopener noreferrer" className="login-link">ورود</a>
+                <a href="#" rel="noopener noreferrer" className="creat-account-link">ثبت نام</a>
             </div>
             <form className="login-creat-account-form">
-                <label for="phone-number">.لطفا شماره موبایل خود را وارد کنید</label>
-                <input type="tel" name="phone-number" className="phone-number"
+                <label for="phone-number"  className="enter-phone-number">.لطفا شماره موبایل خود را وارد کنید</label>
+                <input type="tel" name="phone-number" className="phone-number-input"
                     pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                     required/>
                 <input type="submit" value="ارسال کد تائید" className="verification-code-button"/>
